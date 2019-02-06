cc83bd854a1cba1c6d27bc91618ee6b5ba56b532
Jessica Bunyan <tristan@bunyan.com>
2019-02-06 16:09:44 +0000

    select active scene via clicking

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { active: -1 };
+  }
   render() {
     return (
       <div
@@ -8,7 +12,13 @@ export class Nav extends Component {
           "scene nav-scene active c" + React.Children.count(this.props.children)
         }
       >
-        {this.props.children}
+        {React.Children.map(this.props.children, (child, index) => {
+          return React.cloneElement(child, {
+            key: index,
+            active: this.state.active == index,
+            onClick: () => this.setState({ active: index })
+          });
+        })}
       </div>
     );
   }
--- src/Scene.js
+++ src/Scene.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 
 export class Scene extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return <div className="scene">{this.props.children}</div>;
+    return (
+      <div
+        className={"scene " + (this.props.active == true ? "active" : "")}
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
