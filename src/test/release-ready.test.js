2623a66221946bbb43092cb15561f4cf0e20e263
Jessica Bunyan <tristan@bunyan.com>
2019-02-10 16:19:21 +0000

    only 1 scene visible test

--- src/test/release-ready.test.js
+++ src/test/release-ready.test.js
@@ -4,17 +4,29 @@ import App from "../App";
 import { Inventory } from "../Inventory";
 import { mount, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { Scene } from "./../Scene";
 
 configure({ adapter: new Adapter() });
 
 it("Inventory is empty", () => {
   const div = document.createElement("div");
 
-  var app = ReactDOM.render(<App />, div);
   var app = mount(<App />);
   var inv = app.find(".inventory").first();
 
   var invChildren = inv.children();
   expect(invChildren).toHaveLength(0);
+
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("Story flags are unset, 1 scene visible", () => {
+  const div = document.createElement("div");
+
+  var app = mount(<App />);
+  var nav = app.find(".nav-scene").first();
+
+  var childScenes = nav.children(Scene);
+
+  expect(childScenes).toHaveLength(1);
 });
