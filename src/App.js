d70f2b0bf3cc233b7efdb7cc303c8a1526749873
Jessica Bunyan <tristan@bunyan.com>
2019-02-10 13:41:26 +0000

    initial tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Game } from "./Game";
 
-export class App extends Component {
+class App extends Component {
   render() {
     return <Game />;
   }
 }
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { assert } from "mocha";
+import { hexToRgb } from "./utils";
 
-describe("test test", () => {
-  it("returns value", () => {
-    assert.equal(true, true);
-  });
+it("hexToRGB isCorrect - white", () => {
+  var res = hexToRgb("#ffffff");
+  expect(res.r).toEqual(255);
+  expect(res.g).toEqual(255);
+  expect(res.b).toEqual(255);
 });
 
-// it("renders without crashing", () => {
-//   const div = document.createElement("div");
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it("hexToRGB isCorrect - purple", () => {
+  var res = hexToRgb("#9400D3");
+  expect(res.r).toEqual(148);
+  expect(res.g).toEqual(0);
+  expect(res.b).toEqual(211);
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
