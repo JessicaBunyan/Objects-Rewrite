1cd8c9d17dee0189bf62375dd819a673bb0d65eb
Jessica Bunyan <tristan@bunyan.com>
2019-02-10 13:10:36 +0000

    working on tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { assert } from "mocha";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("test test", () => {
+  it("returns value", () => {
+    assert.equal(true, true);
+  });
 });
+
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
--- src/Game.js
+++ src/Game.js
@@ -6,19 +6,19 @@ import flags from "./storyFlags";
 import * as _ from "underscore";
 import { newVarId } from "./utils";
 
-// var c = {
-//   id: 999,
-//   type: "colour",
-//   value: [9, 1, 9]
-// };
+var c = {
+  id: 999,
+  type: "colour",
+  value: [9, 1, 9]
+};
 
 export class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
       storyFlags: {},
-      // inv: [c]
-      inv: []
+      inv: [c]
+      // inv: []
     };
   }
 
