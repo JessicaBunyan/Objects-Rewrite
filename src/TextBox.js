7b31d8882797736aef1922a0cc84ae09c1777a09
Jessica Bunyan <tristan@bunyan.com>
2019-02-13 15:33:22 +0000

    turned textbox into function

--- src/TextBox.js
+++ src/TextBox.js
@@ -2,12 +2,6 @@ import React, { Component } from "react";
 import { calcColour } from "./utils";
 import colourDrop from "./img/colourDrop.png";
 
-export class TextBox extends Component {
-  render() {
-    return (
-      <div className={" text-box " + this.props.className}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+export const TextBox = ({ className, children }) => {
+  return <div className={" text-box " + className}>{children}</div>;
+};
